13d627e80fff3de90c884be323414ead2d0a5857
Manoj Choudhary <jobidmanoj@gmail.com>
2026-03-01 01:29:14 +0530

    Updated with api.js
    Commit changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://backend-assignment-xspa.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
