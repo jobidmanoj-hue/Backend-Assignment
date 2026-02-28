cb1ab36da05e2f24b8dc26ffb634e4c72f519601
Manoj Choudhary <jobidmanoj@gmail.com>
2026-03-01 01:49:22 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,8 +8,7 @@ API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
-  
-}
+  }
   return req;
 });
 
